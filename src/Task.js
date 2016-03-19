072b3cdc83c3c70b435f909c4a878b412bdfb9f7
Rosy Tucker <rtucker@thoughtworks.com>
2016-03-19 13:40:20 +0000

    Implement task post interface

--- src/Task.js
+++ src/Task.js
@@ -1,6 +1,29 @@
-const endPoint = '/task';
+const endPoint = '/task',
+    destinations = ['LDN', 'MCR'];
+
+const isNumber = (input) => {
+    return typeof input === 'number';
+};
+
+const isValidDestination = (input) => {
+    return destinations.indexOf(input) > -1;
+};
 
 const postHandler = (req, res) => {
+    const {destination, type} = req.body;
+
+    if (!isValidDestination(destination)) {
+        res.statusCode = 400;
+        res.json({data: {error: `Invalid task destination: ${destination}`}});
+        return;
+    }
+
+    if (!isNumber(type)) {
+        res.statusCode = 400;
+        res.json({data: {error: 'Task type must be a number'}});
+        return;
+    }
+
     res.statusCode = 200;
     res.json({data: {}});
 };
--- test/Task.test.js
+++ test/Task.test.js
@@ -1,17 +1,49 @@
 import {expect, sinon} from './TestHelper';
 import Task from '../src/Task';
 
+
+const request = (destination, type) => {
+    return {
+        body: {destination: destination, type: type}
+    };
+};
+
 describe('Task', () => {
+    let response;
+
+    beforeEach(() => {
+        response = {json: sinon.spy()};
+    });
+
     it('should have endpoint of task', () => {
         expect(Task.endPoint).to.equal('/task');
     });
 
-    it('should return 200 when valid task posted', () => {
-        const req = {}, res = {json: sinon.spy()};
+    it('should return 200 when valid task posted for LDN', () => {
+        Task.postHandler(request('LDN', 1), response);
 
-        Task.postHandler(req, res);
+        expect(response.statusCode).to.equal(200);
+        expect(response.json).to.have.been.calledWith({data: {}});
+    });
 
-        expect(res.statusCode).to.equal(200);
-        expect(res.json).to.have.been.calledWith({data: {}});
+    it('should return 200 when valid task posted for MCR', () => {
+        Task.postHandler(request('MCR', 2), response);
+
+        expect(response.statusCode).to.equal(200);
+        expect(response.json).to.have.been.calledWith({data: {}});
+    });
+
+    it('should return 400 when task posted with invalid destination', () => {
+        Task.postHandler(request('NYC', 1), response);
+
+        expect(response.statusCode).to.equal(400);
+        expect(response.json).to.have.been.calledWith({data: {error: 'Invalid task destination: NYC'}});
+    });
+
+    it('should return 400 when task posted with invalid type', () => {
+        Task.postHandler(request('LDN', 'String'), response);
+
+        expect(response.statusCode).to.equal(400);
+        expect(response.json).to.have.been.calledWith({data: {error: 'Task type must be a number'}});
     });
 });
