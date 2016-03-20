53af8bf919b508c6ef833b906384b60c7cc64124
Rosy Tucker <rtucker@thoughtworks.com>
2016-03-20 11:31:25 +0000

    Add body parser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import Task from './src/Task';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.post(Task.endPoint, Task.postHandler);
 
