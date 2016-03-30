cc2e447b83917c12b5e88df06db409572a8b0897
Rosy Tucker <rtucker@thoughtworks.com>
2016-03-30 22:39:41 +0100

    Temp: Add some logs

--- src/SensorReading/SensorReadingStore.js
+++ src/SensorReading/SensorReadingStore.js
@@ -4,6 +4,7 @@ const seriesName = 'SensorReadings';
 
 const putReading = (deviceId, value) => {
     const client = DatabaseClient.getClient();
+    console.log('Adding', deviceId, value, Date.now());
     client.zadd(seriesName, Date.now(), JSON.stringify({deviceId: deviceId, value: value}));
 };
 
@@ -11,6 +12,7 @@ const getLatestReadings = (timeRangeMillis) => {
     return new Promise(resolve => {
         const client = DatabaseClient.getClient();
         client.zrangebyscore(seriesName, Date.now() - timeRangeMillis, Date.now(), (results) => {
+            console.log('Results', results);
             resolve(results);
         });
     });
