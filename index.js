2cd709f2d4704c0c603d07ad2beb23d63fccae24
Rosy Tucker <rtucker@thoughtworks.com>
2016-04-07 21:13:30 +0100

    Add graph UI as static assets

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static('graphUI'));
 
 app.post(Task.endPoint, Task.postHandler);
 app.get(Task.endPoint, Task.getHandler);
